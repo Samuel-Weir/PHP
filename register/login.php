a61d8dacbd517564634eb5ad22684c23bbaed37f
Samuel-Weir <s.weir@ngenroom.co.nz>
2025-05-07 14:32:16 +1200

    Add files via upload

---
+++ register/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+ <div class="container">
+    <div class="row">
+        <div class="col-md-4 offset-md-4 form-div">
+            <form action="signup.php" method="post">
+                <h3 class="text-center">Login</h3>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control form-control-lg">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control form-control-lg">
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="login-btn" class="btn btn-primary btn-block btn-lg">Login</button>
+                </div>
+                <p class="text-center">Not yet a member <a href="signup.php">Sign Up</a></p>
+            </form>
+        </div>
+    </div>
+ </div>
+</body>
+</html>
